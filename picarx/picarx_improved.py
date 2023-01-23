dd135236aa123619d4b28ed08407854edc69fa2c
raff17 <moralera@oregonstate.edu>
2023-01-22 19:16:05 -0800

    added logs

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -191,7 +191,7 @@ class Picarx(object):
         w = (v / r) * (1 - (t / (2 * R)))
         return abs(w)
 
-    @
+
     def backward(self, speed):
         current_angle = self.dir_current_angle
         if current_angle != 0:
