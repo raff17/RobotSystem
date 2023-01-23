bac8616b159055b39b42a3d1a997442772602e2b
raff17 <moralera@oregonstate.edu>
2023-01-22 21:22:38 -0800

    adjust values in action.py (calibration)

--- picarx/actions.py
+++ picarx/actions.py
@@ -6,15 +6,15 @@ class Maneuvering(object):
     def __init__(self):
         self.px = Picarx()
         #self.px = px
-        self.default_speed = 40
-        self.default_steering = 20
-        self.max_steering = 40
+        self.speed = 20  # speed
+        self.steering_angle = 10  # default angle
+        self.max_angle = 40  # max angle
         self.pause = 1
         self.command_wait = 0.25
         atexit.register(self.cleanup)
 
     def calibrate_steering(self):
-        self.px.forward(self.default_speed)
+        self.px.forward(self.speed)
         time.sleep(self.pause)
         self.px.stop()
 
@@ -22,23 +22,12 @@ class Maneuvering(object):
     def menu(self):
         while True:
             print("Welcome to the Picar menu!")
-            print("0: Calibrate Steering")
-            # print("1: Forward and Backward (with steering")
-            # print("2: Parallel Parking")
-            # print("3: K-turn")
+            print("1: Calibrate Steering")
             print("q: Quit")
 
             menu_option = input("Please select a maneuver or q to quit: ")
             if menu_option == "0":
                 maneuvering.calibrate_steering()
-            # elif menu_option == "1":
-            #     maneuvering.forward_and_backward_with_steering()
-            # elif menu_option == "2":
-            #     maneuvering.parallel_parking()
-            # elif menu_option == "3":
-            #     maneuvering.k_turn()
-            # elif menu_option == "q":
-            #     return
             else:
                 print("Invalid Selection")
 
