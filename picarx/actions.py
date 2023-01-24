58d9ec384a461a9bb75d8b1fc7a57fc01280c5c8
raff17 <moralera@oregonstate.edu>
2023-01-24 05:57:36 -0800

    added fixing steering and moving forward in parallel

--- picarx/actions.py
+++ picarx/actions.py
@@ -99,7 +99,7 @@ class Maneuvering(object):
             self.px.set_dir_servo_angle(35)
         time.sleep(self.command_wait)
         self.px.backward(self.speed)
-        time.sleep(.5)
+        time.sleep(1)
         self.px.stop()
         time.sleep(self.command_wait)
         self.px.set_dir_servo_angle(0)
