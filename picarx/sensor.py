80ba307c6fa82d9c3a3c592577dd444f3495295e
raff17 <moralera@oregonstate.edu>
2023-01-26 13:25:18 -0800

    added move code to sensor code

--- picarx/sensor.py
+++ picarx/sensor.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
     while True:
         sensor = Sensors("A0","A1","A2")
         print(sensor)
+
         # ssensor = sensor[0]
         # print(ssensor)
     # if Sensors("A0") < 200:
