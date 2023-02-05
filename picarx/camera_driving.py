8e0cfba0365d1d0786c4bfc6526808cc69e8a5e3
raff17 <moralera@oregonstate.edu>
2023-02-05 13:11:12 -0800

    - 90 again

--- picarx/camera_driving.py
+++ picarx/camera_driving.py
@@ -293,7 +293,7 @@ def lane_following(resolution=(640,480), framerate=24):
         # car.forward(3)
         cv2.imshow("video", edges)
         cv2.imshow("mask", roi)
-        car.constant_move(10, angle/5)
+        car.constant_move(10, angle - 90)
         # Exit if the `esc` key is pressed
         rawCapture.truncate(0)  # Release cache
         k = cv2.waitKey(1) & 0xFF
