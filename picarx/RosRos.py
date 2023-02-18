9204a9e281156c167415bcc89ccac781a205ea1c
raff17 <moralera@oregonstate.edu>
2023-02-17 20:45:50 -0800

    edit RosRos.py

--- picarx/RosRos.py
+++ picarx/RosRos.py
@@ -43,10 +43,6 @@ class Bus:
 
 
 def ensureTuple(value):
-    """
-    Function that wraps an input value in a tuple if it is not already a tuple
-    """
-
     if isinstance(value, tuple):
         value_tuple = value
     else:
@@ -56,12 +52,6 @@ def ensureTuple(value):
 
 
 class ConsumerProducer:
-    """
-    Class that turns a provided function into a service that reads from
-    the input buses, stores the resulting data into the output buses,
-    and watches a set of termination buses for a "True" or non-negative signal, at which
-    point the service shuts down
-    """
 
     @log_on_start(DEBUG, "{name:s}: Starting to create consumer-producer")
     @log_on_error(DEBUG, "{name:s}: Encountered an error while creating consumer-producer")
@@ -88,66 +78,43 @@ class ConsumerProducer:
 
         while True:
 
-            # Check if the loop should terminate
-            # termination_value = self.termination_buses[0].get_message(self.name)
             if self.checkTerminationbuses():
                 break
 
-            # Collect all of the values from the input buses into a list
             input_values = self.collectbusesToValues(self.input_buses)
 
-            # Get the output value or tuple of values corresponding to the inputs
             output_values = self.consumer_producer_function(*input_values)
 
-            # Deal the values into the output buses
             self.dealValuesTobuses(output_values, self.output_buses)
 
-            # Pause for set amount of time
             time.sleep(self.delay)
 
-    # Take in a bus or a tuple of buses, and store their
-    # messages into a list
     @log_on_start(DEBUG, "{self.name:s}: Starting collecting bus values into list")
     @log_on_error(DEBUG, "{self.name:s}: Encountered an error while collecting bus values")
     @log_on_end(DEBUG, "{self.name:s}: Finished collecting bus values")
     def collectbusesToValues(self, buses):
 
-        # Wrap buses in a tuple if it isn't one already
         buses = ensureTuple(buses)
 
-        # Create a list for storing the values in the buses
         values = []
 
-        # Loop over the buses, recording their values
         for p in buses:
             values.append(p.get_message(self.name))
 
         return values
 
-    # Take in  a tuple of values and a tuple of buses, and deal the values
-    # into the buses
     @log_on_start(DEBUG, "{self.name:s}: Starting dealing values into buses")
     @log_on_error(DEBUG, "{self.name:s}: Encountered an error while dealing values into buses")
     @log_on_end(DEBUG, "{self.name:s}: Finished dealing values into buses")
     def dealValuesTobuses(self, values, buses):
 
-        # Wrap buses in a tuple if it isn't one already
         buses = ensureTuple(buses)
 
-        # Handle different combinations of bus and value counts
-
-        # If there is only one bus, then the values should be treated as a
-        # single entity, and wrapped into a tuple for the dealing process
         if len(buses) == 1:
             values = (values,)
-        # If there are multiple buses
         else:
-            # If the values are already presented as a tuple, leave them
             if isinstance(values, tuple):
                 pass
-            # If the values are not already presented as a tuple,
-            # Make a tuple with one entry per bus, all of which are the
-            # equal to the input values
             else:
                 values = tuple([values] * len(buses))
 
@@ -169,11 +136,6 @@ class ConsumerProducer:
 
 
 class Producer(ConsumerProducer):
-    """
-    Special case of the consumer-producer class, that sends values to buses
-    but does not read them
-    """
-
     @log_on_start(DEBUG, "{name:s}: Starting to create producer")
     @log_on_error(DEBUG, "{name:s}: Encountered an error while creating producer")
     @log_on_end(DEBUG, "{name:s}: Finished creating producer")
@@ -186,9 +148,6 @@ class Producer(ConsumerProducer):
         # Producers don't use an input bus
         input_buses = Bus(0, "Default producer input bus")
 
-        # Match naming convention for this class with its parent class
-        # def syntax is necessary because a producer function will not accept
-        # input values
         def consumer_producer_function(_input_value): return producer_function()
 
         # Call the parent class init function
@@ -202,10 +161,6 @@ class Producer(ConsumerProducer):
 
 
 class Consumer(ConsumerProducer):
-    """
-    Special case of the consumer-producer class, that reads values from buses
-    but does not send to them
-    """
 
     @log_on_start(DEBUG, "{name:s}: Starting to create consumer")
     @log_on_error(DEBUG, "{name:s}: Encountered an error while creating consumer")
@@ -233,12 +188,6 @@ class Consumer(ConsumerProducer):
 
 
 class Timer(Producer):
-    """
-    Timer is a producer that keeps track of time since it was instantiated
-    Time is instantiated with a duration value, and writes a "countdown" value (time before or after the
-    specified duration) to its output bus. The output bus can be used as the termination bus for other
-    consumer-producers (and for the timer itself); these consumer producers will terminate when the timer reaches zero
-    """
 
     @log_on_start(DEBUG, "{name:s}: Starting to create timer")
     @log_on_error(DEBUG, "{name:s}: Encountered an error while creating timer")
@@ -275,9 +224,6 @@ class Timer(Producer):
 
 
 class Printer(Consumer):
-    """
-    Printer is a consumer that reads a value stored in a bus and prints it out at specified intervals
-    """
 
     @log_on_start(DEBUG, "{name:s}: Starting to create printer")
     @log_on_error(DEBUG, "{name:s}: Encountered an error while creating printer")
@@ -321,20 +267,12 @@ class Printer(Consumer):
 @log_on_error(DEBUG, "runConcurrently: Encountered an error during concurrent execution")
 @log_on_end(DEBUG, "runConcurrently: Finished concurrent execution")
 def runConcurrently(producer_consumer_list):
-    """
-    runConcurrently is aFunction that uses a concurrent.futures ThreadPoolExecutor to concurrently
-    execute a set of ConsumerProducer functions
-    """
-
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(producer_consumer_list)) as executor:
 
-        # Create a list to hold the executors created from the provided functions
         executor_list = []
 
-        # Loop over the list of provided functions, turning each into an executor for the thread pool
         for cp in producer_consumer_list:
             executor_list.append(executor.submit(cp))
 
-    # Loop over the executors that were created above, running their result methods
     for e in executor_list:
         e.result()
